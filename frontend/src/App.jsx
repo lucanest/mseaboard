361ccb511ac07550e9296c0dc36f83f808133faa
Luca Nesterenko <nesterenko.luca@gmail.com>
2025-06-12 12:25:05 +0200

    github link and temporary(?) removal of dark mode button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -650,9 +650,26 @@ if (type === 'alignment') {
           <button onClick={() => triggerUpload('histogram')} className="bg-orange-200 text-black px-4 py-2 rounded hover:bg-orange-400">
             Upload data (.txt/.tsv/.csv)
           </button>
-          <button onClick={() => setDarkMode(prev => !prev)} className="bg-gray-800 text-white px-2 py-1 rounded hover:bg-gray-700 dark:bg-white dark:text-black dark:hover:bg-gray-300">
+          <a
+            href="https://github.com/lucanest/mseaview"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-flex items-center px-2 py-1 rounded hover:bg-gray-200"
+            title="View on GitHub"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+              className="w-7 h-7 text-gray-800 dark:text-gray-200"
+              aria-hidden="true"
+            >
+              <path d="M12 2C6.477 2 2 6.484 2 12.021c0 4.428 2.867 8.184 6.839 9.504.5.092.682-.217.682-.482 0-.237-.009-.868-.014-1.703-2.782.605-3.369-1.342-3.369-1.342-.454-1.154-1.11-1.462-1.11-1.462-.908-.62.069-.608.069-.608 1.004.07 1.532 1.032 1.532 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.339-2.221-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.254-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.025A9.564 9.564 0 0 1 12 6.844c.85.004 1.705.115 2.504.337 1.909-1.295 2.748-1.025 2.748-1.025.546 1.378.202 2.396.1 2.65.64.7 1.028 1.595 1.028 2.688 0 3.847-2.337 4.695-4.566 4.944.36.31.68.921.68 1.857 0 1.34-.012 2.421-.012 2.751 0 .267.18.578.688.48C19.135 20.2 22 16.447 22 12.021 22 6.484 17.523 2 12 2z"/>
+            </svg>
+          </a>
+          {/*<button onClick={() => setDarkMode(prev => !prev)} className="bg-gray-800 text-white px-2 py-1 rounded hover:bg-gray-700 dark:bg-white dark:text-black dark:hover:bg-gray-300">
             {darkMode ? '☀︎' : '☾'}
-          </button>
+          </button>*/}
           <input ref={fileInputRef} type="file" accept=".fasta,.nwk,.nhx,.txt,.tsv,.csv,.fas" onChange={handleFileUpload} style={{ display: 'none' }} />
         </div>
       </div>
