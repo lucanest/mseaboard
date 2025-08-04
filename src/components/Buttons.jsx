f7a5d48488738fd64563350ff650417f480df13f
Luca Nesterenko <nesterenko.luca@gmail.com>
2025-08-04 22:52:39 +0200

    code cleanup

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -20,9 +20,11 @@ export function RemoveButton({ onClick }) {
     <button
       onClick={onClick}
       className="p-0.5"
-      title="Remove panel">
-<span className="inline-flex items-center justify-center w-6 h-6 rounded bg-gray-200 border border-gray-400 hover:bg-red-300">
-        <XMarkIcon className="w-5 h-5 text-gray-700" />
+      title="Remove panel"
+    >
+      <span className="inline-flex items-center justify-center w-6 h-6
+       rounded bg-gray-200 border border-gray-400 hover:bg-red-300">
+       <XMarkIcon className="w-5 h-5 text-gray-700" />
       </span>
     </button>
   );
@@ -30,25 +32,21 @@ export function RemoveButton({ onClick }) {
 
 export function LinkButton({ onClick, isLinked, isLinkModeActive }) {
   return (
-  <button
+    <button
         onClick={onClick}
         className="p-0.5"
-        title={isLinked ? 'Unlink panels' : 'Link panel'}>
-        <span className={`inline-flex items-center justify-center w-6 h-6 rounded hover:bg-yellow-300
-        ${isLinkModeActive ? 'bg-blue-200' :
-        isLinked         ? 'bg-green-200' :
-                           'bg-gray-200'}
+        title={isLinked ? 'Unlink panels' : 'Link panel'}
+    >
+      <span className={`inline-flex items-center justify-center w-6 h-6 rounded hover:bg-yellow-300
+        ${isLinkModeActive ? 'bg-blue-200' : isLinked ? 'bg-green-200' :'bg-gray-200'}
         border border-gray-400`}>
           <LinkIcon
-          className={`
-            w-5 h-5
-            ${isLinkModeActive ? 'text-blue-700' :
-              isLinked         ? 'text-green-700' :
-                                'text-gray-500'}`}
-            aria-hidden="true"
+          className={`w-5 h-5
+            ${isLinkModeActive ? 'text-blue-700' : isLinked ? 'text-green-700' : 'text-gray-500'}`
+            }
           />
-        </span>
-      </button>
+      </span>
+    </button>
   );
 }
 
@@ -60,7 +58,7 @@ export function RadialToggleButton({ onClick, isActive }) {
       title="Switch tree view"
     >
       <span className={`inline-flex items-center justify-center w-6 h-6 rounded
-      ${isActive ? 'bg-gray-200' : 'bg-gray-200'}
+        ${isActive ? 'bg-gray-200' : 'bg-gray-200'}
         border border-gray-400 hover:bg-orange-300`}>
         <span className="text-xs font-bold text-purple-800 leading-none">
           <EyeIcon className="w-5 h-5" />
